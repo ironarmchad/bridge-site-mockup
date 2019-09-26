b136c5bc75918044279ee75b57a05dd4ce89f461
ironarmchad <mcline@muralab.org>
2019-09-26 12:37:54 -0400

    about 50% done on imnew page, held up by weird box that doesn't contain all elements

--- src/app/imnew/imnew.component.ts
+++ src/app/imnew/imnew.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, Directive, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+
 
 @Component({
   selector: 'app-imnew',
   templateUrl: './imnew.component.html',
   styleUrls: ['./imnew.component.scss']
 })
-export class ImnewComponent implements OnInit {
+export class ImnewComponent implements OnInit, AfterViewInit {
+  topOffset: string;
+  height: string;
+  @ViewChild('startBox', {static: true}) startBox: ElementRef;
+  @ViewChild('startTitle', {static: true}) startTitle: ElementRef;
+  @ViewChild('startVideo', {static: true}) startVideo: ElementRef;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  setBackdrop() {
+    const height = this.startTitle.nativeElement.offsetHeight;
+    this.height = height + 'px';
+    const maxHeight = Math.max(this.startTitle.nativeElement.offsetHeight, this.startVideo.nativeElement.offsetHeight);
+    this.topOffset = `${Math.round(0.5 * (maxHeight - height))}px`;
+    console.log(this.topOffset);
+  }
+
+  ngOnInit(): void {
+    this.setBackdrop();
+    console.log(`Init: ${this.topOffset}`);
+  }
+
+  ngAfterViewInit(): void {
+    this.setBackdrop();
+    console.log(`ViewInit: ${this.topOffset}`);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  resetTop() {
+    this.setBackdrop();
+  }
 }
